6d1d2ba4c6ca64c9bf45c7a9aeb283e47e352532
Gunawan Arya <made2gunawan@gmail.com>
2024-02-22 14:01:16 +0800

    [chore]:setup new monogame repo n follow youtube tutorial

--- TutorialGame1/Game1.cs
+++ TutorialGame1/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace TutorialGame1
 {
@@ -8,6 +9,18 @@ namespace TutorialGame1
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        Texture2D targetSprite;
+        Texture2D crosshairsSprite;
+        Texture2D backgroundSprite;
+        SpriteFont gameFont;
+
+        Vector2 targetPossition = new Vector2(300,300);
+        const int targetRadius = 45;
+
+        MouseState mouseState;
+        bool mouseReleased = true;
+        int score = 0;
 
         public Game1()
         {
@@ -28,6 +41,10 @@ namespace TutorialGame1
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            targetSprite = Content.Load<Texture2D>("target");
+            crosshairsSprite = Content.Load<Texture2D>("crosshairs");
+            backgroundSprite = Content.Load<Texture2D>("sky");
+            gameFont = Content.Load<SpriteFont>("galleryFont");
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,6 +53,26 @@ namespace TutorialGame1
                 Exit();
 
             // TODO: Add your update logic here
+            mouseState = Mouse.GetState();
+
+            if(mouseState.LeftButton == ButtonState.Pressed && mouseReleased == true)
+            {
+                float mouseTargetDistance = Vector2.Distance(targetPossition, mouseState.Position.ToVector2());
+                if(mouseTargetDistance < targetRadius)
+                {
+                    score++;
+
+                    Random random = new Random();
+                    targetPossition.X = random.Next(0, _graphics.PreferredBackBufferWidth);
+                    targetPossition.Y = random.Next(0, _graphics.PreferredBackBufferHeight);
+                }
+                mouseReleased = false;
+            }
+
+            if(mouseState.LeftButton == ButtonState.Released)
+            {
+                mouseReleased = true;
+            }
 
             base.Update(gameTime);
         }
@@ -45,6 +82,11 @@ namespace TutorialGame1
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(backgroundSprite, new Vector2(0, 0), Color.White);
+            _spriteBatch.DrawString(gameFont, score.ToString(), new Vector2(100, 100), Color.White);
+            _spriteBatch.Draw(targetSprite, new Vector2(targetPossition.X - targetRadius , targetPossition.Y - targetRadius), Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
